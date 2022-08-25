d960c6f29acff8c2ef0add88a9f944bda39ec522
Rayan Shareef [Legend] <100436158+RayanShareef07@users.noreply.github.com>
2022-08-25 22:44:13 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -49,6 +49,8 @@ ui_print " ▌PROCESSOR ▰ $(getprop ro.product.board) "
 sleep 0.5
 ui_print ""
 ui_print "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
+ui_print " ▌Activating ▰ Pegasus  ✗ Instagram Reel's Green Lines Fixer "
+ui_print "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ "
 ui_print " "
 ui_print "▰ Join Our Channel's & Group To Support Us"
 ui_print " "
